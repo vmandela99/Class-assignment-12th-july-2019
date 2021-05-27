f260d11f46b23da116fbd8bef7dc5ff9954e4923
victor <vmandela99@gmail.com>
2021-05-27 20:43:34 +0300

    Changes from lesson 3, 2021, May 27th

---
+++ Lesson2.R
@@ -0,0 +1,15 @@
+# Read in the demographics data
+demographics_data <- read.csv("demographics.csv")
+
+# Read the mobile_money_data
+mobile_data <- read.csv("mobilemoney_data.csv") 
+
+#view the data
+mobile_data
+
+# Package tidyverse
+# install.packages("tidyverse")
+
+# load
+library(tidyverse)
+
