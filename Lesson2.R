55fab285494077e66e3419fc65d71a9582a23e5b
victor <vmandela99@gmail.com>
2021-05-27 20:46:25 +0300

    some changes in lesson 3

--- Lesson2.R
+++ Lesson2.R
@@ -1,15 +1,31 @@
-# Read in the demographics data
-demographics_data <- read.csv("demographics.csv")
-
 # Read the mobile_money_data
 mobile_data <- read.csv("mobilemoney_data.csv") 
 
-#view the data
-mobile_data
-
-# Package tidyverse
-# install.packages("tidyverse")
-
-# load
+# load the package
 library(tidyverse)
 
+# Exploratory data analysis (EDA)
+glimpse(mobile_data)
+summary(mobile_data)
+
+
+library(skimr)
+skim(mobile_data)
+
+# check out individual variables
+mobile_data %>% count(account_type, sort = T)
+
+
+# Replacing NA values
+mobile_data <- mobile_data %>% 
+  na_if("") %>% 
+  na_if("-97")
+
+# quick check functions
+names(mobile_data)
+head(mobile_data, 10) # first 10 observations
+tail(mobile_data, 2) # last six observations
+dim(mobile_data) # dimensions of the data set
+ncol(mobile_data) # number of columns
+nrow(mobile_data) # Number of Rows
+length(mobile_data) # variables
